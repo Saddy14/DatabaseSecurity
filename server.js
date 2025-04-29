467d831ba842ee3d91f5168d10e603856714be9e
Saddy14 <1221301874@student.mmu.edu.my>
2025-04-29 15:48:30 +0800

    server.js to include SQL connection and login handling

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const { connect } = require('./db.js')
+const { connect, sql } = require('./db.js')
 
 app.use(bodyParser.json()) //? For JSON data
 app.use(bodyParser.urlencoded({ extended: true })) //? For URL encoded data
@@ -14,14 +14,14 @@ app.use(cors()) //? For CORS
 app.use(express.static(path.join(__dirname, 'Public')));
 
 
-// connect() //? Connect to the database
+connect() //? Connect to the database
 
-//     .then(() => {
-//         console.log("Connected to the database")
-//     })
-//     .catch((err) => {
-//         console.log("Error connecting to the database: " + err)
-//     })
+    .then(() => {
+        console.log("Connected to the database")
+    })
+    .catch((err) => {
+        console.log("Error connecting to the database: " + err)
+    })
 
 
 app.get('/login', (req, res) => {
@@ -31,12 +31,23 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
 
-    res.sendFile(path.join(__dirname, 'Public', 'admin-dashboard.html'));
-
     console.log("Login Form Info:");
     console.log(req.body);
     
-    
+    sql.query(`SELECT * FROM Application.Users WHERE email = '${req.body.email}' AND password = '${req.body.password}'`)
+        .then(result => {
+            if (result.recordset.length > 0) {
+                console.log("User found: " + result.recordset[0].email);
+                res.sendFile(path.join(__dirname, 'Public', 'admin-dashboard.html'));
+            } else {
+                console.log("User not found");
+                res.status(401).send("Invalid email or password");
+            }
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+            res.status(500).send("Error: " + err);
+        });
 
 });
 
@@ -54,6 +65,14 @@ app.get('/contact', (req, res) => {
 
     res.sendFile(path.join(__dirname, 'Public', 'contact.html'));
 });
+
+app.get('/logout', (req, res) => {
+
+    // res.sendFile(path.join(__dirname, 'Public', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'Public', 'login.html'));
+});
+
+
 
 
 
